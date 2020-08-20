f63b8c40b395efacb9700d1692b9158404419d22
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-08-19 19:05:34 -0700

    web page using js

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var username = prompt('Please Enter your Name ');
+
+alert('Hello ' + username);
+
+if (username === "Sowmya"){
+    document.write('<h2>' + 'Welcome Billakanti ' + username + '</h2>')
+} else {
+    document.write('<h2>' + 'Welcome' + username + '</h2>')
+}
