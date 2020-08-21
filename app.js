430b36d8f34def145649a809e5cfc0178f0c4b94
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-08-20 22:23:30 -0700

    To use JS loop

--- app.js
+++ app.js
@@ -1,39 +1,63 @@
-'use strict'
+// 'use strict'
 
-// Wrap this code in a function
+// // Wrap this code in a function
 
-function askCustomerForName(){
-    var username = prompt('Enter your name for a Free Coffee! ');
-    alert('Hello ' + username);
-    return username;
-}
+// function askCustomerForName(){
+//     var username = prompt('Enter your name for a Free Coffee! ');
+//     alert('Hello ' + username);
+//     return username;
+// }
 
-var username = askCustomerForName();
+// var username = askCustomerForName();
 
-console.log(username);
+// console.log(username);
 
-function checkForWinner(){
-    if (username === 'Sowmya'){
-        document.write('<h2>' + 'Congratulations! Here is your free coffee ' + username + '</h2>')
-    } else {
-        document.write('<h2>' + 'OOPS! Better luck next time' + '</h2>')
+// function checkForWinner(){
+//     if (username === 'Sowmya'){
+//         document.write('<h2>' + 'Congratulations! Here is your free coffee ' + username + '</h2>')
+//     } else {
+//         document.write('<h2>' + 'OOPS! Better luck next time' + '</h2>')
+//     }
+// }
+
+// checkForWinner();
+
+
+// function askForRating() {
+//     var rating = prompt('Please give us a rating (1-5):');
+//     return rating;
+// }
+
+// var ratingGivenByUser = askForRating()
+
+// function writeToWebPage(message) {
+//     document.write('<h3>' + message + '</h3>')
+// }
+
+// writeToWebPage('Rating given by you is: ' + ratingGivenByUser)
+
+// writeToWebPage("Thanks for visiting. Please come back again!")
+
+function numberGuessingGame() {
+    var correctAnswer = 37;
+    var playGame = prompt('Do you want to play a game?');
+    while(playGame === 'Yes'){
+        for (var i = 0; i < 5; i = i + 1){
+            var userGuess = parseInt(prompt('Please Enter a number from 1-100'));
+            if(userGuess === correctAnswer){
+                alert('Guessed correct!')
+                break;
+        } else if (userGuess < correctAnswer){
+            alert('You guessed to low')
+        } else if (userGuess > correctAnswer){
+            alert('You guessed to high')
+        }
+        if(i===4){
+            alert('The correct answer is ' + correctAnswer);
+
+        }
     }
+    playGame = prompt('Do you want to play another game?');
 }
-
-checkForWinner();
-
-
-function askForRating() {
-    var rating = prompt('Please give us a rating (1-5):');
-    return rating;
 }
-
-var ratingGivenByUser = askForRating()
-
-function writeToWebPage(message) {
-    document.write('<h3>' + message + '</h3>')
-}
-
-writeToWebPage('Rating given by you is: ' + ratingGivenByUser)
-
-writeToWebPage("Thanks for visiting. Please come back again!")
+numberGuessingGame();
